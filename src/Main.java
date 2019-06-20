eb6279f833b493bddaf2f325cb97be2a6e66ba58
temi <temitopem@gmail.com>
2019-06-20 10:33:27 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num;
+        boolean isPrime;
+        Scanner.keybord = new Scanner;
+
+
+        for (int j=2; j< num; j++);
+            if (num % j == 0) (
+                    System.out.println(num + "divisible by " + j);
+                    isPrime= false;
+
+
+        if (isPrime) {
+            System.out.println(num + "is a prime number."
+        } else {
+            System.out.println(num +
+        )
+        }
+    }
+}
+;
